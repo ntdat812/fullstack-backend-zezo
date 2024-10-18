3ed8c32a5f7a5b5ff59c28f61d2a41ae011e55d7
ntdat <nguyenthanhdatbi812@gmail.com>
2024-10-19 00:33:58 +0700

    viet api create user method post

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -4,11 +4,30 @@ const getUsersAPI = async (req, res) => {
     let results = await User.find({});
 
     return res.status(200).json({
-        errCode: 0,
+        success: true,
         data: results
+    });
+}
+
+
+const postCreateUserAPI = async (req, res) => {
+
+    let { email, name, city } = req.body;
+    //mongoDb
+    let user = await User.create({
+        email: email,
+        name: name,
+        city: city,
+    })
+
+    return res.status(200).json({
+        success: true,
+        data: user
     });
 }
 
 module.exports = {
     getUsersAPI,
+    postCreateUserAPI,
+
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,20 +2,13 @@ const express = require('express');
 
 const routerAPI = express.Router();
 
-const { getUsersAPI } = require('../controllers/apiController');
+const { getUsersAPI, postCreateUserAPI } = require('../controllers/apiController');
 
-routerAPI.get('/', (req, res) => {
-    res.send("Hello world with api");
-});
 
-routerAPI.get('/abc', (req, res) => {
-    //Gồm 2 phần trả ra trạng thái và truyền về data cho client
-    res.status(201).json({
-        data: "xin chao api dau tien",
-    })
-});
 
 routerAPI.get('/users', getUsersAPI);
+
+routerAPI.post('/users', postCreateUserAPI);
 
 
 module.exports = routerAPI;
